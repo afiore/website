565bc6d913739c4e4ae54ae50e32119e38b09634
Andrea Fiore <andrea.giulio.fiore@gmail.com>
2020-02-16 22:23:16 +0000

    add trailing slash to baseurl

--- website/siteConfig.js
+++ website/siteConfig.js
@@ -12,7 +12,7 @@ const siteConfig = {
   title: "Unfold", // Title for your website.
   tagline: "Andrea's software engineering notes",
   url: 'https://afiore.github.io', // Your website URL
-  baseUrl: '/website', // Base URL for your project */
+  baseUrl: '/website/', // Base URL for your project */
   // For github.io type URLs, you would set the url and baseUrl like:
   //   url: 'https://facebook.github.io',
   //   baseUrl: '/test-site/',
